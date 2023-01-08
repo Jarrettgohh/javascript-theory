1f7d4eb43ed1cf9dcd6712a6abd02c83e7cf2616
Jarrett Goh <Jarrettgoh.xz@gmail.com>
2023-01-08 11:20:10 +0800

    Update obj_deep_shallow_copy.js

---
+++ obj_deep_shallow_copy.js
@@ -0,0 +1,25 @@
+const obj1 = {
+  test: "jarrett",
+};
+
+const obj2 = {
+  test: "jarrett",
+};
+
+const obj3 = obj1; // shallow copy - returns true for Object.is()
+
+console.log(obj1 === obj2); // false
+console.log(Object.is(obj1, obj2)); // false
+console.log(Object.is(obj1, obj3)); // true
+console.log(obj1 == obj3); // true
+
+obj3.test = "this will change obj1 too, as its a shallow copy";
+console.log(obj1);
+
+const obj4 = JSON.parse(JSON.stringify(obj2)); // single layer deep copy
+
+obj4.test = "this will not change obj2, as its a deep copy";
+console.log(obj2);
+
+console.log(obj4 === obj2); // false
+console.log(Object.is(obj4, obj2)); // false
